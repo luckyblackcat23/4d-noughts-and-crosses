340025fdf379909ff2f575d2d7a43c2ed2acaa27
jordan <79121452+luckyblackcat23@users.noreply.github.com>
2023-07-05 21:58:59 +0100

    Update GridGenerator.cs

--- GridGenerator.cs
+++ GridGenerator.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class GridGenerator : MonoBehaviour
 {
     public GameObject GridSquare;
-    
+
     public void Start()
     {
-        for(i = 0; i < GameManager.GameArea.x; i++)
+        for (int i = 0; i < GameManager.GameArea.x; i++)
         {
-            for(ii = 0; ii < GameManager.GameArea.y; ii++)
+            for (int ii = 0; ii < GameManager.GameArea.y; ii++)
             {
-                Instantiate(GridSquare, new Quaternion, new Vector2(i - GameArea.x / 2, ii - GameArea.y / 2))
+                Instantiate(GridSquare, new Vector2(i - GameManager.GameArea.x / 2, ii - GameManager.GameArea.y / 2), new Quaternion());
             }
         }
     }
